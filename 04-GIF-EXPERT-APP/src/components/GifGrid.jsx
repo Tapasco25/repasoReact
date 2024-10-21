47bc3c2f3e7d69e2dcbb23caf661b79e93248626
Nicol Tapasco <tapasconicol25@gmail.com>
2024-10-21 08:41:31 -0500

    04-GIF-EXPERT-APP test

--- 04-GIF-EXPERT-APP/src/components/GifGrid.jsx
+++ 04-GIF-EXPERT-APP/src/components/GifGrid.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { GifItem } from "./GifItem";
-import { useFetchGifs } from "../hooks/useFetchGifs";
-// import { getGifs } from "../helpers/getGifs";
+import PropTypes from 'prop-types';
+
+import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-  const { images, isLoading } = useFetchGifs(category);
 
-  
+    const { images, isLoading } = useFetchGifs( category );
+    
+    return (
+        <>
+            <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
+            
 
-  return (
-    <>
-      <h3>{category}</h3>
-      
-      {
-        isLoading && (<h2>Cargando...</h2>)
-      }
+            <div className="card-grid">
+                {
+                    images.map( ( image ) => (
+                        <GifItem 
+                            key={ image.id } 
+                            { ...image }
+                        />
+                    ))
+                }
+                
+            </div>
 
-      <div className="card-grid">
-        {images.map((image) => (
-          <GifItem key={image.id} {...image} />
-        ))}
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
+
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
+}
