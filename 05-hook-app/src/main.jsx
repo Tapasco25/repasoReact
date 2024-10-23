e17f1d6b7cf866368dca2ac38cf3533244ea6168
Nicol Tapasco <tapasconicol25@gmail.com>
2024-10-23 09:10:39 -0500

    05-Hook-app finaliza sesion 9

---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import { Padre } from './07-tarea-memo/Padre'
+import { Hijo } from './07-tarea-memo/Hijo'
+// import { CallbackHook } from './06-memos/CallbackHook'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { Memorize } from './06-memos/Memorize'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Hijo/>
+  </StrictMode>,
+)
