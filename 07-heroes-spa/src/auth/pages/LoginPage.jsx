6ddea0d6e7ad0531c706f1a07a6df382880552c7
Nicol Tapasco <tapasconicol25@gmail.com>
2024-11-12 09:04:09 -0500

    cambio de nombre

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ export const LoginPage = () => {
     
     const lastPath = localStorage.getItem('lastPath') || '/';
 
-    login( 'Fernando Herrera' );
+    login( 'Nicol Tapasco' );
     
     navigate( lastPath, {
       replace: true
